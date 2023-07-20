c61abdbe11a92cb48948ad021167c0aad1ebb454
Tayyub2858 <tayyub2858@gmail.com>
2023-07-20 10:58:27 -0700

    update

--- src/Components/OtherContant/OtherContant.jsx
+++ src/Components/OtherContant/OtherContant.jsx
@@ -7,7 +7,7 @@ const OtherContant = () => {
         <div className="othercontant-container">
             <Grid container>
                 <Grid xs={12} sm={12} md={12}>
-                    <h1 className="heading-contanct">Add Your Other Contant</h1>
+                    <h1 className="heading-contanct">Add Your Other Module of Components</h1>
                 </Grid>
             </Grid>
         </div>
